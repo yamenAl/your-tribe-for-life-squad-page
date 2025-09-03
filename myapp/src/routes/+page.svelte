7f781e73f40a4e49775ebbaba9bf17927ba2fabf
Nayomekaia <nayomedoelwijt@gmail.com>
2025-09-04 00:29:34 +0200

    json data opgehaald
    json data opgehaald op homepagina

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script>
+    export let data;
+    const { localData } = data; // hier word de lokale json data opgehaald
+  </script>
+  
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+  <ul>
+    {#each localData.data as item}
+      <li>{item.name}</li>
+      <p>{item.profilepicture}</p>
+    {/each}
+  </ul>
