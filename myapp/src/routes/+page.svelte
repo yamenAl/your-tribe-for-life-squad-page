2285745f3ac63a3811a9522f677789fd900f56f0
yamenAl <yamen_alsharabi@hotmail.com>
2025-09-04 09:54:59 +0200

    server

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    let { data } = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+{#each members as member}
+<p>{member.name}</p>
+{/each}
+
