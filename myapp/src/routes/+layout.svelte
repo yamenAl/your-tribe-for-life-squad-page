8f8b31e6bbf6123d04cd9faae77fb19007220bf4
Nayomekaia <nayomedoelwijt@gmail.com>
2025-09-11 15:30:07 +0200

    json opgehaald in layout

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 hetzelfde geladen zodat je het niet steeds opnieuw hoeft te doen-->
   
 <header>Mijn header</header>
+<script>
+  import { page, navigating } from '$app/stores';
+  export let data;
+  const { localData } = data; // ik heb de localData uit de data gehaald zodat ik die kan gebruiken
+</script>
 
   <main>
 	<slot /> <!-- dit is een term voor dat de hier de +pages geladen word  -->
