0a4c1a55f7060ffd9fdf06f3a6f85da696d66a8d
Nayomekaia <nayomedoelwijt@gmail.com>
2025-09-11 19:36:10 +0200

    onnodig json ophalen weggehaald

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,15 +1,7 @@
-<script>
-  export let data;
-  const { localData } = data // hier word de lokale json data opgehaald en directus data
-  const members=data.members
-  console.log(members)
-</script>
 
 <!-- dit is default voor de rechter kant wanneer er geen character is gekozen-->
 
-
-<!-- <h1>Selecteer links een member</h1>
-<p>Klik op een naam/foto in de lijst om details te zien.</p>-->
-
+ <h1>Selecteer links een member</h1>
+<p>Klik op een naam/foto in de lijst om details te zien.</p>
 
 
