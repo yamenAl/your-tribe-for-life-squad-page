16f6191fe96fce32f18fda0241647318f666a0a8
yamenAl <yamen_alsharabi@hotmail.com>
2025-09-16 19:35:23 +0200

    addeed labels to open/close menu #22
    Add aria-label="Open menu" and aria-label="Close menu" to improve screen also for the items inside the nav reader support.

--- myapp/src/lib/components/Header.svelte
+++ myapp/src/lib/components/Header.svelte
@@ -1,9 +1,11 @@
+<script>
+  import { HomeLogo, MapLogo, GoBackLogo } from '$lib';
+</script>
 
 <input id="menu-toggle" type="checkbox" hidden>
 
 <header class="site-header">
-  <label for="menu-toggle" class="open-btn" label="Open menu">
-    <!-- OPEN (hamburger) -->
+  <label for="menu-toggle" class="open-btn" label="Open menu" aria-label="Open menu">
     <svg width="74" height="57" viewBox="0 0 74 57" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M67.8332 35.625H70.9165V38H67.8332V40.375H27.7498V38H24.6665V35.625H27.7498V33.25H67.8332V35.625Z" fill="#88E2C1"/>
       <path d="M70.9165 4.75V7.125H67.8332V9.5H27.7498V7.125H24.6665V4.75H27.7498V2.375H67.8332V4.75H70.9165Z" fill="#88E2C1"/>
@@ -14,7 +16,7 @@
 </header>
 
 <nav class="overlay" aria-modal="true" role="dialog">
-  <label for="menu-toggle" class="close-btn" label="Close menu">
+  <label for="menu-toggle" class="close-btn" label="Close menu" aria-label="Close menu">
     <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M26.8749 23.2913H28.6666V25.083H30.4583V26.8747H32.2499V28.6663H34.0416V30.458H35.8333V32.2497H37.6249V34.0413H39.4166V35.833H37.6249V37.6247H35.8333V39.4163H34.0416V37.6247H32.2499V35.833H30.4583V34.0413H28.6666V32.2497H26.8749V30.458H25.0833V28.6663H23.2916V26.8747H19.7083V28.6663H17.9166V30.458H16.1249V32.2497H14.3333V34.0413H12.5416V35.833H10.7499V37.6247H8.95825V39.4163H7.16659V37.6247H5.37492V35.833H3.58325V34.0413H5.37492V32.2497H7.16659V30.458H8.95825V28.6663H10.7499V26.8747H12.5416V25.083H14.3333V23.2913H16.1249V19.708H14.3333V17.9163H12.5416V16.1247H10.7499V14.333H8.95825V12.5413H7.16659V10.7497H5.37492V8.95801H3.58325V7.16634H5.37492V5.37467H7.16659V3.58301H8.95825V5.37467H10.7499V7.16634H12.5416V8.95801H14.3333V10.7497H16.1249V12.5413H17.9166V14.333H19.7083V16.1247H23.2916V14.333H25.0833V12.5413H26.8749V10.7497H28.6666V8.95801H30.4583V7.16634H32.2499V5.37467H34.0416V3.58301H35.8333V5.37467H37.6249V7.16634H39.4166V8.95801H37.6249V10.7497H35.8333V12.5413H34.0416V14.333H32.2499V16.1247H30.4583V17.9163H28.6666V19.708H26.8749V23.2913Z" fill="white"/>
     </svg>
@@ -22,136 +24,87 @@
 
   <ul class="cards">
     <li>
-      <a class="card card--mint" href="/">
-       <svg width="74" height="81" viewBox="0 0 74 81" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_d_75_29)">
-        <path d="M44.4 0H29.6V7.6H22.2V15.2H14.8V22.8H7.4V30.4H0V38H7.4V76H33.3V53.2H40.7V76H66.6V38H74V30.4H66.6V22.8H59.2V15.2H51.8V7.6H44.4V0ZM44.4 7.6V15.2H51.8V22.8H59.2V30.4H66.6V38H59.2V68.4H48.1V45.6H25.9V68.4H14.8V38H7.4V30.4H14.8V22.8H22.2V15.2H29.6V7.6H44.4Z" fill="#3B876B"/>
-        <path d="M14.2998 38.5V68.9004H26.4004V46.0996H47.5996V68.9004H59.7002V38.5H66.0996V75.5H41.2002V52.7002H32.7998V75.5H7.90039V38.5H14.2998ZM73.5 30.9004V37.5H67.0996V30.9004H73.5ZM6.90039 30.9004V37.5H0.5V30.9004H6.90039ZM66.0996 23.2998V29.9004H59.7002V23.2998H66.0996ZM14.2998 23.2998V29.9004H7.90039V23.2998H14.2998ZM58.7002 15.7002V22.2998H52.2998V15.7002H58.7002ZM21.7002 15.7002V22.2998H15.2998V15.7002H21.7002ZM51.2998 8.09961V14.7002H44.9004V8.09961H51.2998ZM29.0996 8.09961V14.7002H22.7002V8.09961H29.0996ZM43.9004 0.5V7.09961H30.0996V0.5H43.9004Z" stroke="black"/>
-        </g>
-        <defs>
-        <filter id="filter0_d_75_29" x="0" y="0" width="74" height="81" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-        <feOffset dy="5"/>
-        <feComposite in2="hardAlpha" operator="out"/>
-        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.75 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_75_29"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_75_29" result="shape"/>
-        </filter>
-        </defs>
-        </svg>
-
-        
-        <span>Home</span>
+      <a href="/" aria-label="Home">
+        <HomeLogo aria-hidden="true" focusable="false" />
       </a>
     </li>
     <li>
-      <a class="card card--yellow" href="/map">
-        <svg width="90" height="93" viewBox="0 0 90 93" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_d_75_33)">
-        <path d="M0 9.86372H10V4.99729H20V0.130859H40V4.99729H55V9.86372H65V4.99729H75V0.130859H90V77.9937H80V82.8601H70V87.7266H50V82.8601H35V77.9937H25V82.8601H15V87.7266H0V9.86372ZM10 19.5966V73.1273H20V68.2608H25V14.7301H15V19.5966H10ZM50 14.7301H35V68.2608H40V73.1273H55V19.5966H50V14.7301ZM70 19.5966H65V73.1273H75V68.2608H80V14.7301H70V19.5966Z" fill="#FFFB96"/>
-        <path d="M39.5 0.630859V5.49707H54.5V10.3633H65.5V5.49707H75.5V0.630859H89.5V77.4941H79.5V82.3604H69.5V87.2266H50.5V82.3604H35.5V77.4941H24.5V82.3604H14.5V87.2266H0.5V10.3633H10.5V5.49707H20.5V0.630859H39.5ZM14.5 19.0967H9.5V73.627H20.5V68.7607H25.5V14.2305H14.5V19.0967ZM34.5 68.7607H39.5V73.627H55.5V19.0967H50.5V14.2305H34.5V68.7607ZM69.5 19.0967H64.5V73.627H75.5V68.7607H80.5V14.2305H69.5V19.0967Z" stroke="black"/>
-        </g>
-        <defs>
-        <filter id="filter0_d_75_33" x="0" y="0.130859" width="90" height="92.5957" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-        <feOffset dy="5"/>
-        <feComposite in2="hardAlpha" operator="out"/>
-        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.75 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_75_33"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_75_33" result="shape"/>
-        </filter>
-        </defs>
-        </svg>
-
-        <span>Map</span>
+      <a href="/map" aria-label="Map">
+        <MapLogo aria-hidden="true" focusable="false" />
       </a>
     </li>
     <li>
-      <label for="menu-toggle" class="card card--rose" role="button">
-        <svg width="99" height="102" viewBox="0 0 99 102" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_d_5_2)">
-        <path d="M31.5 4.3983H22.5V8.79661H18V13.1949H13.5V17.5932H9V21.9915H4.5V30.7881H0V65.9746H4.5V74.7712H9V79.1695H13.5V83.5678H18V87.9661H22.5V92.3644H31.5V96.7627H67.5V92.3644H76.5V87.9661H81V83.5678H85.5V79.1695H90V74.7712H94.5V65.9746H99V30.7881H94.5V21.9915H90V17.5932H85.5V13.1949H81V8.79661H76.5V4.3983H67.5V0H31.5V4.3983ZM67.5 13.1949H76.5V17.5932H81V21.9915H85.5V30.7881H90V65.9746H85.5V74.7712H81V79.1695H76.5V83.5678H67.5V87.9661H31.5V83.5678H22.5V79.1695H18V74.7712H13.5V65.9746H9V30.7881H13.5V21.9915H18V17.5932H22.5V13.1949H31.5V8.79661H67.5V13.1949Z" fill="#E25F7B"/>
-        <path d="M67 0.5V4.89844H76V9.29688H80.5V13.6953H85V18.0928H89.5V22.4912H94V31.2881H98.5V65.4746H94V74.2715H89.5V78.6699H85V83.0674H80.5V87.4658H76V91.8643H67V96.2627H32V91.8643H23V87.4658H18.5V83.0674H14V78.6699H9.5V74.2715H5V65.4746H0.5V31.2881H5V22.4912H9.5V18.0928H14V13.6953H18.5V9.29688H23V4.89844H32V0.5H67ZM31 12.6953H22V17.0928H17.5V21.4912H13V30.2881H8.5V66.4746H13V75.2715H17.5V79.6699H22V84.0674H31V88.4658H68V84.0674H77V79.6699H81.5V75.2715H86V66.4746H90.5V30.2881H86V21.4912H81.5V17.0928H77V12.6953H68V8.29688H31V12.6953Z" stroke="black"/>
-        </g>
-        <g filter="url(#filter1_d_5_2)">
-        <path d="M45 18.334H49.5V22.6265H54V26.9189H49.5V31.2114H45V35.5039H40.5V39.7964H36V44.0889H81V52.6738H36V56.9663H40.5V61.2588H45V65.5513H49.5V69.8438H54V74.1362H49.5V78.4287H45V74.1362H40.5V69.8438H36V65.5513H31.5V61.2588H27V56.9663H22.5V52.6738H18V44.0889H22.5V39.7964H27V35.5039H31.5V31.2114H36V26.9189H40.5V22.6265H45V18.334Z" fill="#E25F7B"/>
-        <path d="M49 18.834V23.127H53.5V26.4189H49V30.7119H44.5V35.0039H40V39.2969H35.5V44.5889H80.5V52.1738H35.5V57.4668H40V61.7588H44.5V66.0518H49V70.3438H53.5V73.6367H49V77.9287H45.5V73.6367H41V69.3438H36.5V65.0518H32V60.7588H27.5V56.4668H23V52.1738H18.5V44.5889H23V40.2969H27.5V36.0039H32V31.7119H36.5V27.4189H41V23.127H45.5V18.834H49Z" stroke="black"/>
-        </g>
-        <defs>
-        <filter id="filter0_d_5_2" x="0" y="0" width="99" height="101.763" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-        <feOffset dy="5"/>
-        <feComposite in2="hardAlpha" operator="out"/>
-        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.75 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_5_2"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_5_2" result="shape"/>
-        </filter>
-        <filter id="filter1_d_5_2" x="18" y="18.334" width="63" height="65.0947" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-        <feOffset dy="5"/>
-        <feComposite in2="hardAlpha" operator="out"/>
-        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.75 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_5_2"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_5_2" result="shape"/>
-        </filter>
-        </defs>
-        </svg>
-
-        <span>Go Back</span>
-      </label>
+      <a href="/" aria-label="Go back">
+        <GoBackLogo aria-hidden="true" focusable="false" />
+      </a>
     </li>
   </ul>
 </nav>
 
 <style>
   
+.site-header {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 1rem;
+}
 
-  .site-header{
-    height:5vh;
-    display:flex;
-    justify-content:flex-end;
-    align-items:center;
-    padding:1rem;
+.open-btn {
+  cursor: pointer;
+}
+
+.overlay {
+  position: fixed;
+  inset: 0;
+  background: var(--dark-blue);
+  display: none;
+  flex-direction: column;  
+  align-items: flex-start;  
+  justify-content: flex-start;
+  padding: 6vh 6vw;
+  overflow-y: auto; 
+  z-index: 9999; 
+  @media (min-width: 900px){
+   align-items: center; 
+  } 
+}
+
+
+
+input[type="checkbox"]:checked ~ .overlay {
+  display: flex;
+}
+
+.close-btn {
+  position: absolute;
+  top: 16px;
+  right: 18px;
+  cursor: pointer;
+}
+
+.cards{
+  list-style:none;
+  display:flex;
+  flex-direction:column;
+  gap:16px;
+  margin:10vh auto 8vh;  
+  padding:0;
+  
+@media (min-width: 900px){
+    display: grid;
+    grid-template-columns: repeat(2, minmax(280px, 1fr));
+    gap: 32px 40px;
+    width: min(820px, 90vw);
+    margin: 12vh auto;        
   }
-  .open-btn{ cursor:pointer; }
-
-  .overlay{
-    position:fixed;
-    inset:0;
-    background:var(--dack-blue);
-    display:none;
-    align-items:center;
-    justify-content:center;
-    padding:6vh 6vw;
+     > li:last-child{
+    grid-column: 1 / -1;
+    justify-self: center;
+    width: min(400px, 60vw);       
   }
-  input[type="checkbox"]:checked ~ .overlay{ display:flex; }
-  input[type="checkbox"]:checked ~ .site-header .open-btn{ display:none; }
-
-  .close-btn{ position:absolute; top:16px; right:18px; cursor:pointer; }
-
-  .cards{
-    list-style:none;
-    margin:0;
-    padding:0;
-    display:flex;
-    flex-direction:column;
-    gap:16px;
+    a{ 
+      display:block; 
+    } 
   }
-
-  .card{
-    color: var(--black);
-    text-decoration:none;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    justify-content:center;
-    padding:2rem 1rem;
-    border-radius:var(--radius);
-    font-weight:700;
-  }
-
 </style>
