e00c6c8c375b894badde4a0ca6702a3a679d4d8a
Nayomekaia <nayomedoelwijt@gmail.com>
2025-09-17 16:08:04 +0200

    classname gegeven aan img

--- myapp/src/routes/[id]/+page.svelte
+++ myapp/src/routes/[id]/+page.svelte
@@ -3,10 +3,18 @@
     const { member, id } = data;
   </script>
   
+  <!--Dit is de rechterkant van de pagina -->
   
+  <section style="text-align: center;">
     <h1>{member.name}</h1>
+    
     {#if member.profilepicture}
-      <img src={member.profilepicture} width="300" alt={member.name} />
-    {/if}
-  </div>
-  
+    <img class="character reveal-up" 
+      src={member.profilepicture} 
+      width="300" 
+      alt={member.name} 
+    />
+  {/if}
+
+  </section>
+
