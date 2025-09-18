f4457d3e7792bbc5c18eef16e4fd1548c7a30df3
yamenAl <yamen_alsharabi@hotmail.com>
2025-09-18 20:28:24 +0200

    new loction for stylesheet #11
    I added the stylesheet in new loction and i import it in layout
    Headings (h1–h4) now have different font sizes and spacing

--- myapp/src/routes/+layout.svelte
+++ myapp/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <!--hier kan je header en footer plaatsen die worden automatich op elke pagina
 hetzelfde geladen zodat je het niet steeds opnieuw hoeft te doen-->
-  
+<script>
+  	import '$lib/assets/stylesheet.css'
+
+</script>
 <header>Mijn header</header>
 
   <main>
